0b7f550de7340464acce12489079316ca6654925
Håkon Stordahl <hakon.stordahl@xledger.com>
2019-01-17 10:36:44 +0100

    Delete unnecessary verbose documentation for simple boilerplate function

--- lib/elixir_fp_intro.ex
+++ lib/elixir_fp_intro.ex
@@ -1,17 +1,4 @@
 defmodule ElixirFpIntro do
-  @moduledoc """
-  Documentation for ElixirFpIntro.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ElixirFpIntro.hello()
-      :world
-
-  """
   def hello do
     :world
   end
